b58307387377da50544ee6c8ab299ba4425321cc
anastasiagh <gheorghita.anastasia@isa.utm.md>
2021-11-05 14:10:00 +0200

    added turnover, training and meeting pop-ups

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import 'react-calendar/dist/Calendar.css';
 import "./style/calendar.css"
 import Auth from "./pages/auth"
 import Calendar from "./pages/calendar"
+import Signin from "./pages/sign_in"
+import Signup from "./pages/sign_up"
+import TurnoverPopUp from "./pages/turnoverPopUp"
 
 
 
 function App({Component}) {
+  
   return (
     <>
-      <Auth />
-      <Calendar />
+      {/* <Auth /> */}
+      <TurnoverPopUp />
+      {/* <Calendar /> */}
     </>
   );
 }
---
+++ src/pages/turnoverPopUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Turnover from "../components/turnover_pop_up"
+
+function turnoverPopUp() {
+    return (
+        <div>
+            <Turnover />
+        </div>
+    )
+}
+
+export default turnoverPopUp
